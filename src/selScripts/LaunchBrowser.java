3be91d717b69a9d7b69a754311d5b4f4e5d4f751
Iaarun <automationbyaarun@gmail.com>
2023-02-26 17:32:12 +0530

    adding browserlaunch

---
+++ src/selScripts/LaunchBrowser.java
@@ -0,0 +1,27 @@
+package selScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+    WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        LaunchBrowser lb = new LaunchBrowser();
+        lb.launchBrowserandBasicFunctions();
+    }
+
+    public void launchBrowserandBasicFunctions() throws InterruptedException {
+        driver= new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+      //  driver.manage().window().minimize();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getWindowHandle());
+
+      //  driver.close();
+
+    }
+}
